592c865e26dabbef8e22ac9642299e9ab6fb811c
Анатолий Тузовский <tuzovskyaf@gmail.com>
2024-02-08 12:11:36 +0300

    «Добавлен оператор Console.ReadKey()»

--- GitLabApp/Program.cs
+++ GitLabApp/Program.cs
@@ -10,6 +10,15 @@ namespace GitLabApp
     {
         static void Main(string[] args)
         {
+            {
+                Console.Write("Введите значение параметра x: ");
+                string s = Console.ReadLine();
+                double x = Convert.ToDouble(s);
+                double F = 2 * Math.Cos(x);
+                Console.WriteLine($"Значение F = {F}.");
+                Console.ReadKey();
+
+            }
         }
     }
 }
